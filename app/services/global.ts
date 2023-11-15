7e238ec0e7af2151b4d297a7466b17155633456b
kabagouda <technodor7@gmail.com>
2023-11-15 21:02:42 +0000

    Refactor GlobalServices class to add new methods for username and platform color

--- app/services/global.ts
+++ app/services/global.ts
@@ -1,7 +1,28 @@
-// add @ to username if not present
-export function addAtToUsername(username: string) {
-  if (username[0] !== "@") {
-    return `@${username}`;
+export class GlobalServices {
+  // add @ to username if not present
+  static addAtToUsername(username: string) {
+    if (username[0] !== "@") {
+      return `@${username}`;
+    }
+    return username;
   }
-  return username;
+
+  static perfectColorOfPlatform(platform: string) {
+    platform = platform.toLowerCase();
+
+    switch (platform) {
+      case "facebook":
+        return "border-blue-600";
+      case "twitter":
+        return "border-blue-400";
+      case "instagram":
+        return "border-pink-500";
+      case "linkedin":
+        return "border-blue-700";
+      case "github":
+        return "dark:border-white";
+      default:
+        return ""; // return a default value in case the platform is not matched
+    }
+  }
 }
