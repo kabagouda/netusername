6f1ea0e3f846e91af1cb6038b9284330e146d19d
kabagouda <technodor7@gmail.com>
2023-11-15 21:02:30 +0000

    Add ApiServices class for checking usernames on social platforms

---
+++ app/services/api-services.ts
@@ -0,0 +1,31 @@
+import { GlobalServices } from "./global";
+
+import { QueryOutput } from "../models/query-output";
+
+export class ApiServices {
+  static async checkUsername(
+    username: string,
+    social: SocialPlatform
+  ): Promise<QueryOutput | null> {
+    username = GlobalServices.addAtToUsername(username);
+    console.log("Called checkUsername for " + username + " on " + social + "");
+    try {
+      let res = await fetch(`${process.env.BASE_URL}/${social}/${username}`);
+      let data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+}
+
+export enum SocialPlatform {
+  Facebook = "facebook",
+  Twitter = "twitter",
+  Instagram = "instagram",
+  Linkedin = "linkedin",
+  Github = "github",
+
+  // add more platforms as needed
+}
