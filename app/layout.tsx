b4cc5e0dac4961c333e3df052354fe68f5712c86
kabagouda <technodor7@gmail.com>
2023-11-16 11:09:39 +0000

    Update title in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Netname",
+  title: "NetUsername",
   description: "Discover social media profiles by username.",
 };
 
